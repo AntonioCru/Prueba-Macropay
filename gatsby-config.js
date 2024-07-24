71d0e9e639802d7f8e30de98933034654b6ba2c9
José Antonio Cruz Soto <antonio.cruz@vantis.mx>
2024-07-23 22:26:50 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ConsultData`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
